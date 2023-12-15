335af00436db579968dae90e95e601ce36961df1
Usmanahmedintern <usman_ahmed_intern@netixsol.com>
2023-12-15 15:58:58 +0500

    update database Issue

--- server/conectToDB.js
+++ server/conectToDB.js
@@ -1,13 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-// Create MongoDB connection Function
-const connectToDB = async(uri) =>{
-    try {
-        await mongoose.connect(uri)
-        console.log("Mongo connected")
-    } catch (error) {
-        console.log(error)
-    }
+// * This is the mongodb Atlas connection link
+const dbConnect = process.env.MONGO_URI;
+
+// * Theses are the parameters
+const connectionParams = {
+    useNewUrlParser: true, useUnifiedTopology: true,
+};
+
+// * This is the mongodb Atlas connection
+const connectToDB = () => {
+    mongoose.connect(dbConnect, connectionParams).then(() => {
+
+        console.log('Hurrah! MongoDB connection successfully established :)');
+
+    }).catch((err) => {
+
+        console.log('Sorry Bro! MongoDB is not connected :(', err);
+
+    })
 }
+
 
 module.exports = connectToDB
