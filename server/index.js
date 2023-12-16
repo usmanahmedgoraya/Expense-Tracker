042745a0b3b7d7a144ec303259e9caf6adb4574f
Usmanahmedintern <usman_ahmed_intern@netixsol.com>
2023-12-16 13:55:26 +0500

    Update the front-end link in index.js cos

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://expense-tracker-front-end-phi.vercel.app"]
+  origin: ["http://localhost:5173", "https://usman-ahmed-expense-tracker.vercel.app"]
 }))
 
 // Declare the port
